6f3a8c4b5f09d3ad99312df396c3511875b687d4
Peter Tessier <fpv5gr@virginia.edu>
2025-02-26 16:04:36 -0500

    add speed

--- src/gdexample.h
+++ src/gdexample.h
@@ -11,6 +11,7 @@ class GDExample : public Sprite2D {
 private:
 	double time_passed;
 	double amplitude;
+    double speed;
 
 protected:
 	static void _bind_methods();
@@ -18,11 +19,14 @@ protected:
 public:
 	GDExample();
 	~GDExample();
-    
+
+	void _process(double delta) override;
+
 	void set_amplitude(const double p_amplitude);
 	double get_amplitude() const;
 
-	void _process(double delta) override;
+    void set_speed(const double p_speed);
+    double get_speed() const;
 };
 
 }
