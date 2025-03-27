b083d4e15df31010ca5021f4d3ef18d6440c6272
Peter Tessier <fpv5gr@virginia.edu>
2025-03-27 17:11:20 -0400

    accidentals are a go

--- demo/scripts/note.gd
+++ demo/scripts/note.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var label: Label = $Label
+@onready var accidental_label: Label = $AccidentalLabel
 
 func _ready():
 	if label == null:
@@ -21,4 +22,9 @@ func set_sprite(duration: int, divisions: int, accidental: int) -> void:
 		1<<7: "𝅗"
 	}
 	var note_char: String = note_chars.get(duration << (6-divisions), '𝅘')
-	label.text = accidental_to_str(accidental) + note_char
+	accidental_label.text = accidental_to_str(accidental)
+	label.text = note_char
+	if accidental < 0:
+		accidental_label.position.y = -611.681
+	if accidental > 0:
+		accidental_label.position.y = -517.016
