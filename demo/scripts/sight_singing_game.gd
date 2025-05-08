8e2e294abc82f163005a7c1ab3a4be40f2705601
Peter Tessier <fpv5gr@virginia.edu>
2025-05-07 21:21:12 -0400

    add song title

--- demo/scripts/sight_singing_game.gd
+++ demo/scripts/sight_singing_game.gd
@@ -1,13 +1,19 @@
+@tool
 extends Control
 
 @export_file("*.xml") var music_xml: String = "res://assets/musicxml/Locriana's Lament.xml"
 @export var music_mp3: AudioStreamMP3
+@export var song_title: String = "Locriana's Lament" :
+	set(value):
+		song_title = value
+		$TitleLabel.text = song_title
 
 @onready var staff_game: Node2D = $StaffGame
 @onready var start_button: Button = $StartButton
 
 func _ready() -> void:
-	staff_game.set_song(music_xml, music_mp3)
+	if not Engine.is_editor_hint():
+		staff_game.set_song(music_xml, music_mp3)
 	
 func _on_start_button_pressed() -> void:
 	start_button.visible = false
