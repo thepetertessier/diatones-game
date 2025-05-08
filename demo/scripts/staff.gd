a795dfa7aceec876beee8b4b2b3a804f02ddbac8
Peter Tessier <fpv5gr@virginia.edu>
2025-05-07 21:07:11 -0400

    customize start button

--- demo/scripts/staff.gd
+++ demo/scripts/staff.gd
@@ -1,10 +1,8 @@
+@tool
 extends Node2D
 
-@onready var staff_line: Line2D = $HorizontalLines/StaffLine
-@onready var staff_line_5: Line2D = $HorizontalLines/StaffLine5
-
 func get_top_staff_y() -> float:
-	return staff_line.position.y
+	return $HorizontalLines/StaffLine.position.y
 
 func get_bottom_staff_y() -> float:
-	return staff_line_5.position.y
+	return $HorizontalLines/StaffLine5.position.y
